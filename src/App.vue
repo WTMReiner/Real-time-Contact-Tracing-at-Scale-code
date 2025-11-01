f11c99e124cface94c5fefb3c9afb360c708b25a
LU Yun <971779052@qq.com>
2025-11-01 23:40:01 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import AMapComponent from './components/AMapComponent.vue';
+
+</script>
+
+<template>
+  <RouterView />
+  <!-- <AMapComponent /> -->
+</template>
+
+<style scoped>
+
+</style>
